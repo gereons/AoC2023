77c36c5da75d33504f44eee293fc7e22614f0d86
Gereon Steffens <gereon@steffens.org>
2023-12-02 10:55:21 +0100

    day 2

--- Sources/Day02/Day02.swift
+++ Sources/Day02/Day02.swift
@@ -6,17 +6,71 @@
 
 import AoCTools
 
+struct Game {
+    let id: Int
+    let sets: [Set]
+
+    struct Set {
+        let red: Int
+        let green: Int
+        let blue: Int
+
+        var power: Int {
+            max(red, 1) * max(green, 1) * max(blue, 1)
+        }
+    }
+
+    init(_ str: String) {
+        let parts = str.components(separatedBy: ": ")
+        self.id = Int(String(parts[0].dropFirst(5)))!
+        var gameSets = [Set]()
+        let sets = parts[1].components(separatedBy: ";")
+        for set in sets {
+            let colors = set.components(separatedBy: ",").map { $0.trimmed() }
+            var red = 0, green = 0, blue = 0
+            for c in colors {
+                let s = c.components(separatedBy: " ")
+                switch s[1] {
+                case "red": red = Int(s[0])!
+                case "green": green = Int(s[0])!
+                case "blue": blue = Int(s[0])!
+                default: fatalError()
+                }
+            }
+            gameSets.append(Set(red: red, green: green, blue: blue))
+        }
+        self.sets = gameSets
+    }
+
+    var isPossible: Bool {
+        sets.allSatisfy {
+            $0.red <= 12 && $0.green <= 13 && $0.blue <= 14
+        }
+    }
+
+    var minimumSet: Set {
+        var red = 0, green = 0, blue = 0
+        sets.forEach {
+            red = max(red, $0.red)
+            green = max(green, $0.green)
+            blue = max(blue, $0.blue)
+        }
+        return Set(red: red, green: green, blue: blue)
+    }
+}
+
 final class Day02: AOCDay {
-    
+    let games: [Game]
+
     init(input: String) {
-        // let input = input ?? Self.input
+        games = input.lines.map { Game($0) }
     }
 
     func part1() -> Int {
-        return 0
+        games.filter { $0.isPossible }.reduce(0) { $0 + $1.id }
     }
 
     func part2() -> Int {
-        return 0
+        games.map { $0.minimumSet.power }.reduce(0, +)
     }
 }
--- Tests/Day02Tests.swift
+++ Tests/Day02Tests.swift
@@ -7,25 +7,30 @@ import XCTest
 
 final class Day02Tests: XCTestCase {
     let testInput = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 """
 
     func testDay02_part1() throws {
         let day = Day02(input: testInput) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 8)
     }
 
     func testDay02_part1_solution() throws {
         let day = Day02(input: Day02.input) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 2149)
     }
 
     func testDay02_part2() throws {
         let day = Day02(input: testInput)
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 2286)
     }
 
     func testDay02_part2_solution() throws {
         let day = Day02(input: Day02.input) 
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 71274)
     }
 }
