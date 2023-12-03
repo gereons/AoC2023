4a8adf0afd3108a88e291acf8069b649e855c881
Gereon Steffens <gereon@steffens.org>
2023-12-03 11:27:20 +0100

    day 3

--- Sources/Day03/Day03.swift
+++ Sources/Day03/Day03.swift
@@ -6,17 +6,90 @@
 
 import AoCTools
 
+struct Number: Hashable {
+    let value: Int
+    let start: Point
+    let length: Int
+    let neighbors: Set<Point>
+
+    init(value: Int, start: Point, length: Int) {
+        self.value = value
+        self.start = start
+        self.length = length
+        neighbors = Set(
+            (start.y - 1 ... start.y + 1).flatMap { y in
+                (start.x - 1 ... start.x + length).map { x in
+                    Point(x, y)
+                }
+            }
+        )
+    }
+}
+
 final class Day03: AOCDay {
-    
+    let symbols: Set<Point>
+    let gears: Set<Point>
+    let numbers: Set<Number>
+
     init(input: String) {
-        // let input = input ?? Self.input
+        var symbols = Set<Point>()
+        var gears = Set<Point>()
+        var numbers = Set<Number>()
+
+        for (y, line) in input.lines.enumerated() {
+            for (x, ch) in line.enumerated() {
+                if !(ch.isNumber || ch == ".") {
+                    symbols.insert(Point(x, y))
+                    if ch == "*" {
+                        gears.insert(Point(x, y))
+                    }
+                }
+            }
+
+            let digits = line.enumerated().filter { $1.isNumber }
+            if digits.isEmpty { continue }
+
+            var value = digits[0].element.wholeNumberValue!
+            var start = digits[0].offset
+            var prevOffset = start
+            var length = 1
+            for d in digits.dropFirst() {
+                if d.offset == prevOffset + 1 {
+                    value = value * 10 + d.element.wholeNumberValue!
+                    length += 1
+                    prevOffset = d.offset
+                } else {
+                    numbers.insert(Number(value: value, start: Point(start, y), length: length))
+                    value = d.element.wholeNumberValue!
+                    length = 1
+                    start = d.offset
+                    prevOffset = start
+                }
+            }
+            numbers.insert(Number(value: value, start: Point(start, y), length: length))
+        }
+
+        self.symbols = symbols
+        self.gears = gears
+        self.numbers = numbers
     }
 
     func part1() -> Int {
-        return 0
+        numbers
+            .filter { !symbols.intersection($0.neighbors).isEmpty }
+            .map { $0.value }
+            .reduce(0, +)
     }
 
     func part2() -> Int {
-        return 0
+        gears
+            .map { gear in
+                numbers.filter { $0.neighbors.contains(gear) }
+            }
+            .filter { $0.count == 2 }
+            .map { number in
+                number.map { $0.value }.reduce(1, *)
+            }
+            .reduce(0, +)
     }
 }
--- Tests/Day03Tests.swift
+++ Tests/Day03Tests.swift
@@ -7,25 +7,35 @@ import XCTest
 
 final class Day03Tests: XCTestCase {
     let testInput = """
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
 """
 
     func testDay03_part1() throws {
         let day = Day03(input: testInput) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 4361)
     }
 
     func testDay03_part1_solution() throws {
         let day = Day03(input: Day03.input) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 557705)
     }
 
     func testDay03_part2() throws {
         let day = Day03(input: testInput)
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 467835)
     }
 
     func testDay03_part2_solution() throws {
         let day = Day03(input: Day03.input) 
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 84266818)
     }
 }
