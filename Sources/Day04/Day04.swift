a147a783ee24a6aab2c1ba779a6f9526e846cb16
Gereon Steffens <gereon@steffens.org>
2023-12-04 09:06:54 +0100

    day 4

--- Sources/Day04/Day04.swift
+++ Sources/Day04/Day04.swift
@@ -5,18 +5,51 @@
 //
 
 import AoCTools
+import Foundation
+
+struct Card {
+    let id: Int
+    let winners: Set<Int>
+    let numbers: Set<Int>
+
+    init(_ string: String) {
+        // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        let parts = string.components(separatedBy: ":")
+        self.id = parts[0].allInts().first!
+        let numbers = parts[1].components(separatedBy: "|")
+        self.winners = Set(numbers[0].allInts())
+        self.numbers = Set(numbers[1].allInts())
+    }
+
+    var matches: Int {
+        numbers.intersection(winners).count
+    }
+
+    var points: Int {
+        1 << (matches - 1) // pow(2, matches - 1)
+    }
+}
 
 final class Day04: AOCDay {
-    
+    let cards: [Card]
+
     init(input: String) {
-        // let input = input ?? Self.input
+        cards = input.lines.map { Card($0) }
     }
 
     func part1() -> Int {
-        return 0
+        cards.map { $0.points }.reduce(0, +)
     }
 
     func part2() -> Int {
-        return 0
+        var copies = [Int](repeating: 1, count: cards.count + 1)
+        copies[0] = 0
+
+        for card in cards where card.matches > 0 {
+            for i in card.id + 1 ... card.id + card.matches where i < copies.count + 1 {
+                copies[i] += copies[card.id]
+            }
+        }
+        return copies.reduce(0, +)
     }
 }
