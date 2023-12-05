df18303338a4c083047648f906fdbd3662879857
Gereon Steffens <gereon@steffens.org>
2023-12-05 10:17:54 +0100

    day 5; part 5 is way too slow

--- Sources/Day05/Day05.swift
+++ Sources/Day05/Day05.swift
@@ -6,17 +6,84 @@
 
 import AoCTools
 
+private struct Almanac {
+    let seeds: [Int]
+
+    let maps: [Map]
+
+    init(_ lines: [String]) {
+        seeds = lines[0].allInts()
+        let chunks = Array(lines.dropFirst(2)).grouped { $0.isEmpty }
+
+        maps = chunks.map { Map($0) }
+    }
+
+    func location(for seed: Int) -> Int {
+        var seed = seed
+        for map in maps {
+            if let range = map.ranges.first(where: { $0.contains(seed) }) {
+                seed = seed - range.src + range.dest
+            }
+        }
+        return seed
+    }
+}
+
+private struct Range {
+    let dest: Int
+    let src: Int
+    let length: Int
+
+    init(_ string: String) {
+        let ints = string.allInts()
+        self.dest = ints[0]
+        self.src = ints[1]
+        self.length = ints[2]
+    }
+
+    func contains(_ x: Int) -> Bool {
+        x >= src && x < (src + length)
+    }
+}
+
+private struct Map {
+    let name: String
+    let ranges: [Range]
+
+    init(_ lines: [String]) {
+        self.name = lines[0]
+        self.ranges = lines.dropFirst().map { Range($0) }
+    }
+}
+
 final class Day05: AOCDay {
-    
+    private let almanac: Almanac
+
     init(input: String) {
-        // let input = input ?? Self.input
+        almanac = Almanac(input.lines)
     }
 
     func part1() -> Int {
-        return 0
+        var minLocation = Int.max
+        for seed in almanac.seeds {
+            let location = almanac.location(for: seed)
+            minLocation = min(minLocation, location)
+        }
+        return minLocation
     }
 
     func part2() -> Int {
-        return 0
+        let seedRanges = almanac.seeds
+            .chunked(2)
+            .map { $0[0] ..< $0[0]+$0[1] }
+
+        var minLocation = Int.max
+        for range in seedRanges {
+            for seed in range {
+                let location = almanac.location(for: seed)
+                minLocation = min(minLocation, location)
+            }
+        }
+        return minLocation
     }
 }
--- Tests/Day05Tests.swift
+++ Tests/Day05Tests.swift
@@ -7,25 +7,58 @@ import XCTest
 
 final class Day05Tests: XCTestCase {
     let testInput = """
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
 """
 
     func testDay05_part1() throws {
         let day = Day05(input: testInput) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 35)
     }
 
     func testDay05_part1_solution() throws {
         let day = Day05(input: Day05.input) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 535088217)
     }
 
     func testDay05_part2() throws {
         let day = Day05(input: testInput)
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 46)
     }
 
     func testDay05_part2_solution() throws {
         let day = Day05(input: Day05.input) 
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 51399228)
     }
 }
