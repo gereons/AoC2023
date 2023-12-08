4068a563b1ea429a3e31fce6756b8074b471499e
Gereon Steffens <gereon@steffens.org>
2023-12-08 10:15:19 +0100

    day 8

--- Sources/Day08/Day08.swift
+++ Sources/Day08/Day08.swift
@@ -5,18 +5,63 @@
 //
 
 import AoCTools
+import RegexBuilder
+
+private struct Node: Hashable {
+    let name: String
+    let left: String
+    let right: String
+
+    init(_ string: String) {
+        let parts = string.components(separatedBy: " = ")
+        name = parts[0]
+        let leftright = parts[1].dropFirst().dropLast().components(separatedBy: ", ")
+        left = leftright[0]
+        right = leftright[1]
+    }
+}
 
 final class Day08: AOCDay {
-    
+    let path: String
+    private let nodes: [String: Node]
+
     init(input: String) {
-        // let input = input ?? Self.input
+        let lines = input.lines
+        self.path = lines[0]
+        let nodes = lines.dropFirst(2).map { Node($0) }
+        self.nodes = nodes.mapped(by: \.name)
     }
 
     func part1() -> Int {
-        return 0
+        let start = nodes["AAA"]!
+        return pathLength(for: start, targetReached: { $0.name == "ZZZ" })
     }
 
     func part2() -> Int {
-        return 0
+        let nodes = nodes.values.filter { $0.name.hasSuffix("A") }
+        let lengths = nodes.map {
+            pathLength(for: $0, targetReached: { $0.name.hasSuffix("Z") })
+        }
+        return lengths.reduce(into: 1) {
+            $0 = lcm($0, $1)
+        }
+    }
+
+    private func pathLength(for node: Node, targetReached: (Node) -> Bool) -> Int {
+        var steps = 0
+        var node = node
+        while true {
+            for direction in path {
+                steps += 1
+                switch direction {
+                case "R": node = nodes[node.right]!
+                case "L": node = nodes[node.left]!
+                default: fatalError()
+                }
+                if targetReached(node) {
+                    return steps
+                }
+            }
+        }
     }
 }
--- Tests/Day08Tests.swift
+++ Tests/Day08Tests.swift
@@ -6,26 +6,58 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day08Tests: XCTestCase {
-    let testInput = """
+    let testInput1a = """
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
 """
 
+    let testInput1b = """
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+"""
+
+    let testInput2 = """
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+"""
     func testDay08_part1() throws {
-        let day = Day08(input: testInput) 
-        XCTAssertEqual(day.part1(), 0)
+        let day1a = Day08(input: testInput1a)
+        XCTAssertEqual(day1a.part1(), 2)
+
+        let day1b = Day08(input: testInput1b)
+        XCTAssertEqual(day1b.part1(), 6)
     }
 
     func testDay08_part1_solution() throws {
         let day = Day08(input: Day08.input) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 19637)
     }
 
     func testDay08_part2() throws {
-        let day = Day08(input: testInput)
-        XCTAssertEqual(day.part2(), 0)
+        let day = Day08(input: testInput2)
+        XCTAssertEqual(day.part2(), 6)
     }
 
     func testDay08_part2_solution() throws {
         let day = Day08(input: Day08.input) 
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 8811050362409)
     }
 }
