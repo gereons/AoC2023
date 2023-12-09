1ffd61e248d92191574ef159aba6508470d94970
Gereon Steffens <gereon@steffens.org>
2023-12-09 08:48:51 +0100

    day 9

--- Sources/Day09/Day09.swift
+++ Sources/Day09/Day09.swift
@@ -7,16 +7,49 @@
 import AoCTools
 
 final class Day09: AOCDay {
-    
+    let sequences: [[Int]]
+
     init(input: String) {
-        // let input = input ?? Self.input
+        sequences = input.lines.map { $0.allInts() }
     }
 
     func part1() -> Int {
-        return 0
+        sequences.map { findNext(in: $0) }.reduce(0, +)
     }
 
     func part2() -> Int {
-        return 0
+        sequences.map { findPrevious(in: $0) }.reduce(0, +)
+    }
+
+    private func findNext(in sequence: [Int]) -> Int {
+        let deltas = getDeltas(for: sequence)
+        var last = deltas.last!.last!
+        for i in (0 ..< deltas.count - 1).reversed() {
+            last = deltas[i].last! + last
+        }
+        return sequence.last! + last
+    }
+
+    private func findPrevious(in sequence: [Int]) -> Int {
+        let deltas = getDeltas(for: sequence)
+        var first = deltas.last!.first!
+        for i in (0 ..< deltas.count - 1).reversed() {
+            first = deltas[i].first! - first
+        }
+        return sequence.first! - first
+    }
+
+    private func getDeltas(for sequence: [Int]) -> [[Int]] {
+        var deltas = [[Int]]()
+        var seq = sequence
+        while true {
+            let delta = seq.adjacentPairs().map { $0.1 - $0.0 }
+            deltas.append(delta)
+            if delta.allSatisfy({ $0 == 0 }) {
+                break
+            }
+            seq = delta
+        }
+        return deltas
     }
 }
--- Tests/Day09Tests.swift
+++ Tests/Day09Tests.swift
@@ -7,25 +7,28 @@ import XCTest
 
 final class Day09Tests: XCTestCase {
     let testInput = """
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
 """
 
     func testDay09_part1() throws {
         let day = Day09(input: testInput) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 114)
     }
 
     func testDay09_part1_solution() throws {
         let day = Day09(input: Day09.input) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 1842168671)
     }
 
     func testDay09_part2() throws {
         let day = Day09(input: testInput)
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 2)
     }
 
     func testDay09_part2_solution() throws {
         let day = Day09(input: Day09.input) 
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 903)
     }
 }
