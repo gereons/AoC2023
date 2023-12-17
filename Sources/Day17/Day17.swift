58408e28d0cfe497e1b58dda32d907862329857a
Gereon Steffens <gereon@steffens.org>
2023-12-17 13:03:28 +0100

    day 17

--- Sources/Day17/Day17.swift
+++ Sources/Day17/Day17.swift
@@ -6,17 +6,92 @@
 
 import AoCTools
 
+private struct State: Hashable {
+    let position: Point
+    let direction: Direction?
+    let chainLength: Int
+}
+
+private class City: Pathfinding {
+    typealias Coordinate = State
+
+    private let grid: [[Int]]
+    private let chainRange: ClosedRange<Int>
+
+    init(grid: [[Int]], chainRange: ClosedRange<Int>) {
+        self.grid = grid
+        self.chainRange = chainRange
+    }
+
+    func neighbors(for state: State) -> [State] {
+        var result = [State]()
+        for dir in Direction.orthogonal {
+            if let direction = state.direction {
+                if direction == dir.opposite {
+                    continue
+                }
+                if direction == dir && state.chainLength == chainRange.upperBound {
+                    continue
+                }
+                if direction != dir && state.chainLength < chainRange.lowerBound {
+                    continue
+                }
+            }
+            let position = state.position.moved(to: dir)
+            if !isInside(position) {
+                continue
+            }
+            let chainLength = dir == state.direction ? state.chainLength + 1 : 1
+            result.append(State(position: position, direction: dir, chainLength: chainLength))
+        }
+        return result
+    }
+
+    func goalReached(at current: State, goal: State) -> Bool {
+        current.position == goal.position && current.chainLength >= chainRange.lowerBound
+    }
+
+    func costToMove(from: State, to: State) -> Int {
+        grid[to.position.y][to.position.x]
+    }
+
+    func distance(from: State, to: State) -> Int {
+        1
+    }
+
+    private func isInside(_ point: Point) -> Bool {
+        0 ..< grid.count ~= point.y && 0 ..< grid[0].count ~= point.x
+    }
+}
+
 final class Day17: AOCDay {
-    
+    private let grid: [[Int]]
+    private var maxX: Int { grid[0].count }
+    private var maxY: Int { grid.count }
+
     init(input: String) {
-        // let input = input ?? Self.input
+        grid = input.lines.map { $0.map { $0.wholeNumberValue! }}
     }
 
     func part1() -> Int {
-        return 0
+        let city = City(grid: grid, chainRange: 0...3)
+        let pathfinder = AStarPathfinder(map: city)
+
+        let start = State(position: .zero, direction: nil, chainLength: 0)
+        let goal = State(position: Point(maxX - 1, maxY - 1), direction: nil, chainLength: 0)
+        let path = pathfinder.shortestPath(from: start, to: goal)
+
+        return path.map { grid[$0.position.y][$0.position.x] }.reduce(0, +)
     }
 
     func part2() -> Int {
-        return 0
+        let city = City(grid: grid, chainRange: 4...10)
+        let pathfinder = AStarPathfinder(map: city)
+
+        let start = State(position: .zero, direction: nil, chainLength: 0)
+        let goal = State(position: Point(maxX - 1, maxY - 1), direction: nil, chainLength: 0)
+        let path = pathfinder.shortestPath(from: start, to: goal)
+
+        return path.map { grid[$0.position.y][$0.position.x] }.reduce(0, +)
     }
 }
--- Tests/Day17Tests.swift
+++ Tests/Day17Tests.swift
@@ -7,25 +7,51 @@ import XCTest
 
 final class Day17Tests: XCTestCase {
     let testInput = """
+2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533
+"""
+
+    let testInput2 = """
+111111111111
+999999999991
+999999999991
+999999999991
+999999999991
 """
 
     func testDay17_part1() throws {
         let day = Day17(input: testInput) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 102)
     }
 
     func testDay17_part1_solution() throws {
         let day = Day17(input: Day17.input) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 1238)
     }
 
-    func testDay17_part2() throws {
+    func testDay17_part2a() throws {
         let day = Day17(input: testInput)
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 94)
+    }
+
+    func testDay17_part2b() throws {
+        let day = Day17(input: testInput2)
+        XCTAssertEqual(day.part2(), 71)
     }
 
     func testDay17_part2_solution() throws {
         let day = Day17(input: Day17.input) 
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 1362)
     }
 }
