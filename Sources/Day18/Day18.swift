ee4e5e11ead7e65c41863f58384e4d8482222f05
Gereon Steffens <gereon@steffens.org>
2023-12-18 08:22:42 +0100

    day 18 part 1

--- Sources/Day18/Day18.swift
+++ Sources/Day18/Day18.swift
@@ -6,17 +6,86 @@
 
 import AoCTools
 
+private struct Dig {
+    let direction: Direction
+    let length: Int
+    let color: UInt32
+
+    static let dirMap: [String: Direction] = ["R": .e, "L": .w, "U": .n, "D": .s]
+
+    init(_ string: String) {
+        // R 6 (#70c710)
+        let parts = string.components(separatedBy: " ")
+        direction = Self.dirMap[parts[0]]!
+        length = Int(parts[1])!
+        color = UInt32(parts[2].substring(2, 6), radix: 16)!
+    }
+}
+
 final class Day18: AOCDay {
-    
+    private let digs: [Dig]
+
     init(input: String) {
-        // let input = input ?? Self.input
+        digs = input.lines.map { Dig($0) }
     }
 
     func part1() -> Int {
-        return 0
+        var point = Point.zero
+        var map = Set([point])
+        for dig in digs {
+            for _ in 0 ..< dig.length {
+                point = point.moved(to: dig.direction)
+                map.insert(point)
+            }
+        }
+
+        // draw(map)
+        let start = findStart(in: map)
+        floodFill(map: &map, from: start)
+        return map.count
     }
 
     func part2() -> Int {
         return 0
+    }
+
+    private func findStart(in map: Set<Point>) -> Point {
+        // find the topmost "left corner" that looks like
+        // ##     <- minY
+        // #.
+        let minY = map.min(of: \.y)!
+        let line1 = map.filter { $0.y == minY + 1 }.sorted(by: \.x)
+        let start = line1.first!.moved(to: .e)
+        assert(map.contains(start.moved(to: .n)))
+        assert(map.contains(start.moved(to: .w)))
+        assert(map.contains(start.moved(to: .nw)))
+        return start
+    }
+
+    private func draw(_ map: Set<Point>) {
+        let maxX = map.max(of: \.x)!
+        let minX = map.min(of: \.x)!
+        let maxY = map.max(of: \.y)!
+        let minY = map.min(of: \.y)!
+
+        for y in minY ... maxY {
+            for x in minX ... maxX {
+                let ch = map.contains(Point(x, y)) ? "#" : "."
+                print(ch, terminator: "")
+            }
+            print()
+        }
+        print("---")
+    }
+
+    private func floodFill(map: inout Set<Point>, from start: Point) {
+        var queue = [start]
+
+        while let point = queue.popLast() {
+            if map.contains(point) { continue }
+            map.insert(point)
+            let neighbors = point.neighbors().filter { !map.contains($0) }
+            queue.append(contentsOf: neighbors)
+        }
     }
 }
--- Tests/Day18Tests.swift
+++ Tests/Day18Tests.swift
@@ -7,21 +7,35 @@ import XCTest
 
 final class Day18Tests: XCTestCase {
     let testInput = """
+R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)
 """
 
     func testDay18_part1() throws {
         let day = Day18(input: testInput) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 62)
     }
 
     func testDay18_part1_solution() throws {
         let day = Day18(input: Day18.input) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 56678)
     }
 
     func testDay18_part2() throws {
         let day = Day18(input: testInput)
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 952408144115)
     }
 
     func testDay18_part2_solution() throws {
