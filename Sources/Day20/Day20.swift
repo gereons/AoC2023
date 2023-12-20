9595f0e2495441a8c838a3044d2ee5180b8d9ad2
Gereon Steffens <gereon@steffens.org>
2023-12-20 11:37:18 +0100

    day 20 part 1

--- Sources/Day20/Day20.swift
+++ Sources/Day20/Day20.swift
@@ -6,14 +6,140 @@
 
 import AoCTools
 
+private enum Pulse {
+    case high, low
+}
+
+private struct Message: CustomStringConvertible {
+    let pulse: Pulse
+    let from: String
+    let destination: String
+
+    var description: String {
+        "\(from) -\(pulse)-> \(destination)"
+    }
+}
+
+private class CommunicationsModule {
+    let name: String
+    let destinations: [String]
+
+    init(name: String, destinations: [String]) {
+        self.name = name
+        self.destinations = destinations
+    }
+
+    func receive(_ pulse: Pulse, from name: String) -> [Message] { [] }
+
+    static func make(from string: String) -> CommunicationsModule {
+        let parts = string.components(separatedBy: " -> ")
+        let prefix = parts[0].prefix(1)
+        let name = String(parts[0].dropFirst(["%", "&"].contains(prefix) ? 1 : 0))
+        let destinations = parts[1].components(separatedBy: ", ")
+
+        switch prefix {
+        case "%": return Flipflop(name: name, destinations: destinations)
+        case "&": return Conjunction(name: name, destinations: destinations)
+        default: return Broadcaster(name: name, destinations: destinations)
+        }
+    }
+}
+
+private class Flipflop: CommunicationsModule {
+    private var state: Bool = false
+
+    override func receive(_ pulse: Pulse, from name: String) -> [Message] {
+        if pulse == .high {
+            return []
+        }
+
+        state.toggle()
+        let pulse: Pulse = state ? .high : .low
+        return destinations.map {
+            Message(pulse: pulse, from: self.name, destination: $0)
+        }
+    }
+}
+
+private class Conjunction: CommunicationsModule {
+    private var inputs = [String: Pulse]()
+
+    func addInput(_ name: String) {
+        inputs[name] = .low
+    }
+
+    override func receive(_ pulse: Pulse, from name: String) -> [Message] {
+        inputs[name] = pulse
+        var send = Pulse.high
+        if inputs.values.allSatisfy({ $0 == .high }) {
+            send = .low
+        }
+        return destinations.map {
+            Message(pulse: send, from: self.name, destination: $0)
+        }
+    }
+}
+
+private class Broadcaster: CommunicationsModule {
+    override func receive(_ pulse: Pulse, from name: String) -> [Message] {
+        destinations.map { Message(pulse: pulse, from: self.name, destination: $0) }
+    }
+}
+
+private class Output: CommunicationsModule {
+}
+
 final class Day20: AOCDay {
-    
+    private let modules: [String: CommunicationsModule]
+
     init(input: String) {
-        // let input = input ?? Self.input
+        let rawModules = input.lines.map { CommunicationsModule.make(from: $0) }
+
+        let conjunctions = rawModules.compactMap { $0 as? Conjunction }
+        for con in conjunctions {
+            for module in rawModules {
+                if module.destinations.contains(con.name) {
+                    con.addInput(module.name)
+                }
+            }
+        }
+
+        var modules = rawModules.mapped(by: \.name)
+        for module in rawModules {
+            for dest in module.destinations {
+                if modules[dest] == nil {
+                    modules[dest] = Output(name: dest, destinations: [])
+                }
+            }
+        }
+        self.modules = modules
     }
 
     func part1() -> Int {
-        return 0
+        let broadcaster = modules["broadcaster"]!
+
+        var highCount = 0
+        var lowCount = 0
+
+        for _ in 0 ..< 1000 {
+            lowCount += 1
+            var messages = broadcaster.receive(.low, from: "btn")
+            lowCount += messages.count
+
+            while !messages.isEmpty {
+                var next = [Message]()
+                for msg in messages {
+                    let msgs = modules[msg.destination]!.receive(msg.pulse, from: msg.from)
+                    let high = msgs.filter { $0.pulse == .high }.count
+                    highCount += high
+                    lowCount += msgs.count - high
+                    next.append(contentsOf: msgs)
+                }
+                messages = next
+            }
+        }
+
+        return highCount * lowCount
     }
 
     func part2() -> Int {
--- Tests/Day20Tests.swift
+++ Tests/Day20Tests.swift
@@ -6,21 +6,36 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day20Tests: XCTestCase {
-    let testInput = """
+    let testInput1 = """
+broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a
+"""
+    let testInput2 = """
+broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output
 """
 
     func testDay20_part1() throws {
-        let day = Day20(input: testInput) 
-        XCTAssertEqual(day.part1(), 0)
+        let day1 = Day20(input: testInput1)
+        XCTAssertEqual(day1.part1(), 32000000)
+
+//        let day2 = Day20(input: testInput2)
+//        XCTAssertEqual(day2.part1(), 11687500)
     }
 
     func testDay20_part1_solution() throws {
         let day = Day20(input: Day20.input) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 879834312)
     }
 
     func testDay20_part2() throws {
-        let day = Day20(input: testInput)
+        let day = Day20(input: testInput1)
         XCTAssertEqual(day.part2(), 0)
     }
 
