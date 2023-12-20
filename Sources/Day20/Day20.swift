c71eab11644e04833da6204fb5d1342ae55d407c
Gereon Steffens <gereon@steffens.org>
2023-12-20 13:29:31 +0100

    day 19 part 2

--- Sources/Day20/Day20.swift
+++ Sources/Day20/Day20.swift
@@ -31,6 +31,8 @@ private class CommunicationsModule {
 
     func receive(_ pulse: Pulse, from name: String) -> [Message] { [] }
 
+    func reset() {}
+
     static func make(from string: String) -> CommunicationsModule {
         let parts = string.components(separatedBy: " -> ")
         let prefix = parts[0].prefix(1)
@@ -45,8 +47,12 @@ private class CommunicationsModule {
     }
 }
 
-private class Flipflop: CommunicationsModule {
+private final class Flipflop: CommunicationsModule {
     private var state: Bool = false
+
+    override func reset() {
+        state = false
+    }
 
     override func receive(_ pulse: Pulse, from name: String) -> [Message] {
         if pulse == .high {
@@ -61,11 +67,19 @@ private class Flipflop: CommunicationsModule {
     }
 }
 
-private class Conjunction: CommunicationsModule {
+private final class Conjunction: CommunicationsModule {
     private var inputs = [String: Pulse]()
+    private(set) var triggered = false
 
     func addInput(_ name: String) {
         inputs[name] = .low
+    }
+
+    override func reset() {
+        triggered = false
+        inputs.keys.forEach {
+            inputs[$0] = .low
+        }
     }
 
     override func receive(_ pulse: Pulse, from name: String) -> [Message] {
@@ -74,13 +88,16 @@ private class Conjunction: CommunicationsModule {
         if inputs.values.allSatisfy({ $0 == .high }) {
             send = .low
         }
+        if !triggered {
+            triggered = send == .high
+        }
         return destinations.map {
             Message(pulse: send, from: self.name, destination: $0)
         }
     }
 }
 
-private class Broadcaster: CommunicationsModule {
+private final class Broadcaster: CommunicationsModule {
     override func receive(_ pulse: Pulse, from name: String) -> [Message] {
         destinations.map { Message(pulse: pulse, from: self.name, destination: $0) }
     }
@@ -116,6 +133,8 @@ final class Day20: AOCDay {
     }
 
     func part1() -> Int {
+        modules.values.forEach { $0.reset() }
+
         let broadcaster = modules["broadcaster"]!
 
         var highCount = 0
@@ -143,6 +162,39 @@ final class Day20: AOCDay {
     }
 
     func part2() -> Int {
-        return 0
+        modules.values.forEach { $0.reset() }
+
+        // find the input to rx
+        let rxSender = modules.values.first { $0.destinations == ["rx" ] }!
+
+        // find its input conjunctions so we can monitor them
+        let inputs = modules.values.filter { $0.destinations.contains(rxSender.name) }.compactMap { $0 as? Conjunction }
+        var counts = [String: Int]()
+
+        let broadcaster = modules["broadcaster"]!
+
+    loop:
+        for presses in 1 ..< Int.max {
+            var messages = broadcaster.receive(.low, from: "btn")
+
+            while !messages.isEmpty {
+                var next = [Message]()
+                for msg in messages {
+                    let msgs = modules[msg.destination]!.receive(msg.pulse, from: msg.from)
+                    next.append(contentsOf: msgs)
+                    for input in inputs {
+                        if input.triggered && counts[input.name] == nil {
+                            counts[input.name] = presses
+                        }
+                    }
+                    if counts.count == inputs.count {
+                        break loop
+                    }
+                }
+                messages = next
+            }
+        }
+        
+        return counts.values.reduce(1, *)
     }
 }
--- Tests/Day20Tests.swift
+++ Tests/Day20Tests.swift
@@ -25,8 +25,8 @@ broadcaster -> a
         let day1 = Day20(input: testInput1)
         XCTAssertEqual(day1.part1(), 32000000)
 
-//        let day2 = Day20(input: testInput2)
-//        XCTAssertEqual(day2.part1(), 11687500)
+        let day2 = Day20(input: testInput2)
+        XCTAssertEqual(day2.part1(), 11687500)
     }
 
     func testDay20_part1_solution() throws {
@@ -34,13 +34,8 @@ broadcaster -> a
         XCTAssertEqual(day.part1(), 879834312)
     }
 
-    func testDay20_part2() throws {
-        let day = Day20(input: testInput1)
-        XCTAssertEqual(day.part2(), 0)
-    }
-
     func testDay20_part2_solution() throws {
         let day = Day20(input: Day20.input) 
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 243037165713371)
     }
 }
