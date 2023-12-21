9a38739a1a39e65c758bee5c7b5cc3b32344601d
Gereon Steffens <gereon@steffens.org>
2023-12-21 10:43:48 +0100

    use infinite grid

--- Sources/Day21/Day21.swift
+++ Sources/Day21/Day21.swift
@@ -6,9 +6,37 @@
 
 import AoCTools
 
-final class Day21: AOCDay {
-    let grid: [[Character]]
+private struct Grid {
+    private let grid: [[Character]]
     let start: Point
+
+    init(_ string: String) {
+        var grid = string.lines.map { $0.map { $0 } }
+        let startY = grid.firstIndex { $0.contains("S") }!
+        let startX = grid[startY].firstIndex { $0 == "S" }!
+        grid[startY][startX] = "."
+        self.grid = grid
+        self.start = Point(startX, startY)
+    }
+
+    subscript(index: Point) -> Character {
+        let x = map(index.x, grid[0].count)
+        let y = map(index.y, grid.count)
+        return grid[y][x]
+    }
+
+    private func map(_ value: Int, _ max: Int) -> Int {
+        let mod = value % max
+        if mod < 0 {
+            let v2 = max + mod
+            return v2 == max ? 0 : v2
+        }
+        return mod
+    }
+}
+
+final class Day21: AOCDay {
+    private let grid: Grid
     let maxSteps: Int
 
     convenience init(input: String) {
@@ -16,54 +44,55 @@ final class Day21: AOCDay {
     }
 
     init(input: String, maxSteps: Int) {
-        var grid = input.lines.map { $0.map { $0 } }
-        let startY = grid.firstIndex { $0.contains("S") }!
-        let startX = grid[startY].firstIndex { $0 == "S" }!
-        grid[startY][startX] = "."
-        self.grid = grid
-        self.start = Point(startX, startY)
+        self.grid = Grid(input)
         self.maxSteps = maxSteps
     }
 
     func part1() -> Int {
-        var steps = [[Set<Int>]](repeating: [Set<Int>](repeating: [0], count: grid[0].count), count: grid.count)
+        var steps = [Point: Set<Int>]()
 
-        floodFill(grid, &steps, from: start, step: 1, max: maxSteps)
+        floodFill(grid, &steps, from: grid.start, step: 1, max: maxSteps)
         // draw(grid: grid, steps: steps)
-        return steps.flatMap { $0 }.filter { $0.contains(maxSteps) }.count
+        return steps.values.compactMap { $0 }.filter { $0.contains(maxSteps) }.count
     }
 
     func part2() -> Int {
         return 0
     }
 
-    private func draw(grid: [[Character]], steps: [[Set<Int>]]) {
-        for y in 0..<steps.count {
-            for x in 0..<steps[0].count {
-                if grid[y][x] == "#" {
+    private func draw(grid: Grid, steps: [Point: Set<Int>]) {
+        let minX = steps.keys.min(of: \.x)!
+        let maxX = steps.keys.max(of: \.x)!
+        let minY = steps.keys.min(of: \.y)!
+        let maxY = steps.keys.max(of: \.y)!
+
+        for y in minY ... maxY {
+            for x in minX ... maxX {
+                let p = Point(x, y)
+                if grid[p] == "#" {
                     print("#", terminator: "")
                 } else {
-                    print(steps[y][x].contains(maxSteps) ? "O" : ".", terminator: "")
+                    print(steps[p]?.contains(maxSteps) == true ? "O" : ".", terminator: "")
                 }
             }
             print()
         }
     }
 
-    private func floodFill(_ grid: [[Character]], _ steps: inout[[Set<Int>]], from start: Point, step: Int, max: Int) {
+    private func floodFill(_ grid: Grid, _ steps: inout [Point: Set<Int>], from start: Point, step: Int, max: Int) {
         if step > max {
             return
         }
 
         let neighbors = start.neighbors().filter {
-            0 ..< grid[0].count ~= $0.x && 0 ..< grid.count ~= $0.y && grid[$0.y][$0.x] == "."
+            grid[$0] == "."
         }
 
         for n in neighbors {
-            if steps[n.y][n.x].contains(step) {
+            if steps[n]?.contains(step) == true {
                 continue
             }
-            steps[n.y][n.x].insert(step)
+            steps[n, default: []].insert(step)
             floodFill(grid, &steps, from: n, step: step + 1, max: max)
         }
     }
--- Tests/Day21Tests.swift
+++ Tests/Day21Tests.swift
@@ -30,6 +30,20 @@ final class Day21Tests: XCTestCase {
         XCTAssertEqual(day.part1(), 3722)
     }
 
+    func testDay21_infiniteGrid() throws {
+        var day = Day21(input: testInput, maxSteps: 10)
+        XCTAssertEqual(day.part1(), 50)
+
+        day = Day21(input: testInput, maxSteps: 50)
+        XCTAssertEqual(day.part1(), 1594)
+
+        day = Day21(input: testInput, maxSteps: 100)
+        XCTAssertEqual(day.part1(), 6536)
+        
+//        day = Day21(input: testInput, maxSteps: 500)
+//        XCTAssertEqual(day.part1(), 167004)
+    }
+
     func testDay21_part2() throws {
         let day = Day21(input: testInput)
         XCTAssertEqual(day.part2(), 0)
