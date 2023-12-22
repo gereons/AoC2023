cf3ba8feb4df1a7dbc41c518c260230e2ba2c997
Gereon Steffens <gereon@steffens.org>
2023-12-22 11:40:19 +0100

    day 22 part 1

--- Sources/Day22/Day22.swift
+++ Sources/Day22/Day22.swift
@@ -6,14 +6,104 @@
 
 import AoCTools
 
+private struct Brick {
+    let start: Point3
+    let end: Point3
+
+    init(_ string: String) {
+        let parts = string.components(separatedBy: "~")
+        let s = parts[0].allInts()
+        let e = parts[1].allInts()
+        start = Point3(s[0], s[1], s[2])
+        end = Point3(e[0], e[1], e[2])
+    }
+
+    init(start: Point3, end: Point3) {
+        self.start = start
+        self.end = end
+    }
+
+    func dropped() -> Brick {
+        Brick(start: Point3(start.x, start.y, start.z - 1),
+              end: Point3(end.x, end.y, end.z - 1))
+    }
+
+    func step(_ path: KeyPath<Point3, Int>) -> Int {
+        let s = start[keyPath: path]
+        let e = start[keyPath: path]
+        return s == e ? 1 : (e - s).signum()
+    }
+
+    var volume: [Point3] {
+        var vol = [Point3]()
+        for x in stride(from: start.x, through: end.x, by: step(\.x)) {
+            for y in stride(from: start.y, through: end.y, by: step(\.y)) {
+                for z in stride(from: start.z, through: end.z, by: step(\.z)) {
+                    vol.append(Point3(x, y, z))
+                }
+            }
+        }
+        return vol
+    }
+
+    var minZ: Int { min(start.z, end.z) }
+    var maxZ: Int { max(start.z, end.z) }
+
+    func isDirectlyBelow(_ other: Brick) -> Bool {
+        self.maxZ == other.minZ - 1
+    }
+}
+
 final class Day22: AOCDay {
-    
+    private let bricks: [Brick]
+
     init(input: String) {
-        // let input = input ?? Self.input
+        bricks = input.lines.map { Brick($0) }
     }
 
     func part1() -> Int {
-        return 0
+        // drop all bricks down as far as possible
+        var volume = Set<Point3>()
+        var droppedBricks = [Brick]()
+        for brick in bricks.sorted(by: { $0.minZ < $1.minZ }) {
+            var brick = brick
+            while true {
+                let dropped = brick.dropped()
+                if dropped.minZ > 0 && volume.intersection(dropped.volume).isEmpty {
+                    brick = dropped
+                } else {
+                    break
+                }
+            }
+            volume.formUnion(brick.volume)
+            droppedBricks.append(brick)
+        }
+
+        // find removable bricks
+        var removable = 0
+
+        for (index, brick) in droppedBricks.enumerated() {
+            var supportsOther = false
+            for brick2 in droppedBricks.dropFirst(index + 1) {
+                if !brick.isDirectlyBelow(brick2) {
+                    continue
+                }
+                let dropped = brick2.dropped()
+                if dropped.minZ < 1 {
+                    continue
+                }
+                let test = volume.subtracting(brick.volume).subtracting(brick2.volume)
+                if test.intersection(dropped.volume).isEmpty {
+                    supportsOther = true
+                    break
+                }
+            }
+            if !supportsOther {
+                removable += 1
+            }
+        }
+
+        return removable
     }
 
     func part2() -> Int {
--- Tests/Day22Tests.swift
+++ Tests/Day22Tests.swift
@@ -7,16 +7,23 @@ import XCTest
 
 final class Day22Tests: XCTestCase {
     let testInput = """
+1,0,1~1,2,1
+0,0,2~2,0,2
+0,2,3~2,2,3
+0,0,4~0,2,4
+2,0,5~2,2,5
+0,1,6~2,1,6
+1,1,8~1,1,9
 """
 
     func testDay22_part1() throws {
         let day = Day22(input: testInput) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 5)
     }
 
     func testDay22_part1_solution() throws {
         let day = Day22(input: Day22.input) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 428)
     }
 
     func testDay22_part2() throws {
