2b40e45191fe4155aa1df1d2ce1255360514bb6b
Gereon Steffens <gereon@steffens.org>
2023-12-23 11:07:51 +0100

    day 23 part 1

--- Sources/Day23/Day23.swift
+++ Sources/Day23/Day23.swift
@@ -5,18 +5,86 @@
 //
 
 import AoCTools
+// paths (.), forest (#), and steep slopes (^, >, v, and <).
+private enum Tile: Equatable {
+    case path
+    case forest
+    case slope(Direction)
+
+    init(_ ch: Character) {
+        switch ch {
+        case ".": self = .path
+        case "#": self = .forest
+        case "^": self = .slope(.n)
+        case "<": self = .slope(.w)
+        case ">": self = .slope(.e)
+        case "v": self = .slope(.s)
+        default: fatalError()
+        }
+    }
+}
 
 final class Day23: AOCDay {
-    
+    private let maze: [[Tile]]
+    private let start: Point
+    private let end: Point
+
     init(input: String) {
-        // let input = input ?? Self.input
+        maze = input.lines.map { $0.map { Tile($0) } }
+        let startX = maze[0].firstIndex { $0 == .path }!
+        start = Point(startX, 0)
+        let endX = maze[maze.count - 1].firstIndex { $0 == .path }!
+        end = Point(endX, maze.count - 1)
     }
 
     func part1() -> Int {
-        return 0
+        findLongestPath(canClimb: false)
     }
 
     func part2() -> Int {
-        return 0
+        findLongestPath(canClimb: true)
+    }
+
+    private func findLongestPath(canClimb: Bool) -> Int {
+        var pathLengths = [Int]()
+        dfs(from: start, to: end, canClimb: canClimb, [], [], &pathLengths)
+
+        return pathLengths.max()!
+    }
+
+    private func dfs(from start: Point, to end: Point, canClimb: Bool, _ visited: Set<Point>, _ currentPath: [Point], _ pathLengths: inout [Int]) {
+        if start == end {
+            let pathLength = currentPath.count
+            pathLengths.append(pathLength)
+            return
+        }
+
+        var visited = visited
+        var currentPath = currentPath
+        visited.insert(start)
+        for n in neighbors(of: start, canClimb: canClimb) {
+            if visited.contains(n) { continue }
+            currentPath.append(n)
+            dfs(from: n, to: end, canClimb: canClimb, visited, currentPath, &pathLengths)
+            currentPath.removeLast()
+        }
+        visited.remove(start)
+    }
+
+    private func neighbors(of point: Point, canClimb: Bool) -> [Point] {
+        var neighbors = [Point]()
+        for direction in [Direction.n, .s, .e, .w] {
+            let np = point.moved(to: direction)
+            if np.x < 0 || np.x >= maze[0].count || np.y < 0 || np.y >= maze.count { continue }
+            switch maze[np.y][np.x] {
+            case .path: neighbors.append(np)
+            case .forest: continue
+            case .slope(let slopeDir):
+                if canClimb || slopeDir == direction {
+                    neighbors.append(np)
+                }
+            }
+        }
+        return neighbors
     }
 }
--- Tests/Day23Tests.swift
+++ Tests/Day23Tests.swift
@@ -7,21 +7,44 @@ import XCTest
 
 final class Day23Tests: XCTestCase {
     let testInput = """
+#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#
 """
 
     func testDay23_part1() throws {
         let day = Day23(input: testInput) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 94)
     }
 
     func testDay23_part1_solution() throws {
         let day = Day23(input: Day23.input) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 1998)
     }
 
     func testDay23_part2() throws {
         let day = Day23(input: testInput)
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 154)
     }
 
     func testDay23_part2_solution() throws {
