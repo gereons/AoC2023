79014e0e5876f559d129ecda4ead6ddd3313b643
Gereon Steffens <gereon@steffens.org>
2023-12-24 11:41:30 +0100

    day 24 part 1

--- Sources/Day24/Day24.swift
+++ Sources/Day24/Day24.swift
@@ -6,14 +6,67 @@
 
 import AoCTools
 
+
+private struct Stone {
+    typealias P3 = (x: Double, y: Double, z: Double)
+
+    let p: P3
+    let v: P3
+
+    // 19, 13, 30 @ -2,  1, -2
+    init(_ string: String) {
+        let parts = string.components(separatedBy: "@")
+        let pos = parts[0].allInts().map { Double($0) }
+        let vel = parts[1].allInts().map { Double($0) }
+        p = (pos[0], pos[1], pos[2])
+        v = (vel[0], vel[1], vel[2])
+    }
+}
+
 final class Day24: AOCDay {
-    
-    init(input: String) {
-        // let input = input ?? Self.input
+    private let stones: [Stone]
+    private let range: ClosedRange<Double>
+
+    convenience init(input: String) {
+        self.init(input: input, range: 200000000000000.0 ... 400000000000000.0)
+    }
+
+    init(input: String, range: ClosedRange<Double>) {
+        stones = input.lines.map { Stone($0) }
+        self.range = range
     }
 
     func part1() -> Int {
-        return 0
+        // https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+        var total = 0
+        for pair in stones.combinations(of: 2) {
+            let s1 = pair[0]
+            let s2 = pair[1]
+            let (slope1, intercept1) = slopeIntercept(for: s1)
+            let (slope2, intercept2) = slopeIntercept(for: s2)
+
+            if slope1 == slope2 {
+                continue // lines are parallel or identical
+            }
+
+            let xCross = (intercept2 - intercept1) / (slope1 - slope2)
+            let yCross = slope1 * xCross + intercept1
+            if range ~= xCross && range ~= yCross {
+                let tCross1 = (xCross - s1.p.x) / s1.v.x
+                let tCross2 = (xCross - s2.p.x) / s2.v.x
+                if tCross1 >= 0 && tCross2 >= 0 {
+                    total += 1
+                }
+            }
+        }
+
+        return total
+    }
+
+    private func slopeIntercept(for stone: Stone) -> (slope: Double, intercept: Double) {
+        let slope = stone.v.y / stone.v.x
+        let intercept = stone.p.y - slope * stone.p.x
+        return (slope, intercept)
     }
 
     func part2() -> Int {
--- Tests/Day24Tests.swift
+++ Tests/Day24Tests.swift
@@ -7,16 +7,21 @@ import XCTest
 
 final class Day24Tests: XCTestCase {
     let testInput = """
+19, 13, 30 @ -2,  1, -2
+18, 19, 22 @ -1, -1, -2
+20, 25, 34 @ -2, -2, -4
+12, 31, 28 @ -1, -2, -1
+20, 19, 15 @  1, -5, -3
 """
 
     func testDay24_part1() throws {
-        let day = Day24(input: testInput) 
-        XCTAssertEqual(day.part1(), 0)
+        let day = Day24(input: testInput, range: 7.0 ... 27.0) 
+        XCTAssertEqual(day.part1(), 2)
     }
 
     func testDay24_part1_solution() throws {
         let day = Day24(input: Day24.input) 
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 26657)
     }
 
     func testDay24_part2() throws {
