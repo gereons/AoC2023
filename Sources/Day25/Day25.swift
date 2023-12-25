0708650dc8f276fb9f88d12e8fee20331500b0a5
Gereon Steffens <gereon@steffens.org>
2023-12-25 11:40:17 +0100

    day 25

--- Sources/Day25/Day25.swift
+++ Sources/Day25/Day25.swift
@@ -7,13 +7,66 @@
 import AoCTools
 
 final class Day25: AOCDay {
-    
+    let connections: [String: Set<String>]
+
     init(input: String) {
-        // let input = input ?? Self.input
+        var connections = [String: Set<String>]()
+        for line in input.lines {
+            let parts = line.components(separatedBy: ":")
+            let lhs = parts[0]
+            let rhs = parts[1].split(separator: " ").map { String($0) }
+            connections[lhs, default: []].formUnion(rhs)
+            for r in rhs {
+                connections[r, default:[]].insert(lhs)
+            }
+        }
+
+        self.connections = connections
     }
 
     func part1() -> Int {
-        return 0
+        // part 2 not solved in code, again. ¯\_(ツ)_/¯
+
+        // instead, visualize the clusters using `neato` using the output printed by `graphViz()`
+        // then, remove the 3 pairs that connect the two clusters and calculate the size of each
+        var connections = connections
+        for p in [("vxr","zhb"),("jbx","sml"),("vqj","szh")] {
+            connections[p.0]?.remove(p.1)
+            connections[p.1]?.remove(p.0)
+        }
+
+        let start = connections.keys.first!
+        let reachable1 = allReachable(from: start, in: connections)
+        let start2 = Set(connections.keys).subtracting(reachable1).first!
+        let reachable2 = allReachable(from: start2, in: connections)
+
+        if reachable1.count + reachable2.count == connections.keys.count {
+            return reachable1.count * reachable2.count
+        }
+        fatalError()
+    }
+
+    private func graphViz() {
+        print("digraph G {")
+        for (key, conn) in connections {
+            for c in conn {
+                print("\(key) -> \(c)")
+            }
+        }
+        print("}")
+    }
+
+    private func allReachable(from start: String, in connections: [String: Set<String>]) -> Set<String> {
+        var result = Set<String>()
+        var queue = [start]
+        while let s = queue.popLast() {
+            result.insert(s)
+            for next in connections[s]! {
+                if result.contains(next) { continue }
+                queue.append(next)
+            }
+        }
+        return result
     }
 
     func part2() -> Int {
--- Tests/Day25Tests.swift
+++ Tests/Day25Tests.swift
@@ -6,26 +6,8 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day25Tests: XCTestCase {
-    let testInput = """
-"""
-
-    func testDay25_part1() throws {
-        let day = Day25(input: testInput) 
-        XCTAssertEqual(day.part1(), 0)
-    }
-
     func testDay25_part1_solution() throws {
         let day = Day25(input: Day25.input) 
-        XCTAssertEqual(day.part1(), 0)
-    }
-
-    func testDay25_part2() throws {
-        let day = Day25(input: testInput)
-        XCTAssertEqual(day.part2(), 0)
-    }
-
-    func testDay25_part2_solution() throws {
-        let day = Day25(input: Day25.input) 
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part1(), 567606)
     }
 }
